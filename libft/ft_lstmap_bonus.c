f3462f2ef07d47905e30f0d901bcd3f5ddf72196
Imane Abasalah <imane4abas@gmail.com>
2024-09-27 11:54:43 +0100

    finally

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,19 +6,18 @@
 /*   By: iabasala <iabasala@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 08:31:05 by iabasala          #+#    #+#             */
-/*   Updated: 2024/09/27 11:07:24 by iabasala         ###   ########.fr       */
+/*   Updated: 2024/09/27 11:33:50 by iabasala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// Function to append a node to the list
 void	ft_lstadd_back(t_list **lst, t_list *new_node)
 {
-	t_list *temp;
+	t_list	*temp;
 
 	if (!lst || !new_node)
-		return;
+		return ;
 	if (!*lst)
 		*lst = new_node;
 	else
@@ -48,7 +47,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		}
 		new_node->content = f(lst->content);
 		new_node->next = NULL;
-		ft_lstadd_back(&new_lst, new_node); // Use helper function to add the new node
+		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
 	return (new_lst);
